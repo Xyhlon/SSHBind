1ff35a8c26414dde998fa89f3d52d9a2859ec267
Maximilian Philipp <maxkon2000@gmail.com>
2025-03-24 01:11:35 +0100

    2fa testing and prompt handling

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -330,3 +330,86 @@ async fn serial_integration_test_second_server_wrong_credentials(
     service_handle.abort();
     Ok(())
 }
+
+#[tokio::test]
+#[serial]
+async fn serial_integration_test_correct_configuration_2fa(
+) -> Result<(), Box<dyn std::error::Error>> {
+    #[allow(clippy::let_unit_value)]
+    let _ = *LOGGER; // Ensure logger is initialized
+    let mut testcreds = YamlCreds::new();
+    testcreds.insert(
+        "127.0.0.1:2222".to_string(),
+        Creds {
+            username: "pi".to_string(),
+            password: "max".to_string(),
+            totp_key: "GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ".to_string().into(),
+        },
+    );
+    let tmp_dir = helpers::setup_sopsfile(testcreds.clone());
+    let sopsfile_path = tmp_dir.path().join("secrets.yaml");
+    let mut config = russh::server::Config::default();
+
+    // Use OsRng from rand for randomness.
+    use russh::keys::ssh_key::rand_core::OsRng;
+    let mut rng = OsRng;
+    use russh::keys::Algorithm;
+    let pk = russh::keys::PrivateKey::random(&mut rng, Algorithm::Ed25519).unwrap();
+    config.keys.push(pk);
+    let config = Arc::new(config);
+    use helpers::Credentials;
+    let mut hosts_users: HashMap<String, HashMap<String, Credentials>> = HashMap::new();
+    testcreds.iter().for_each(|(k, v)| {
+        let mut map: HashMap<String, Credentials> = HashMap::new();
+        map.insert(v.username.clone(), v.clone().into());
+        hosts_users.insert(k.to_string(), map);
+    });
+
+    println!("hosts_users: {:?}", hosts_users);
+
+    let bind_addr = "127.0.0.1:7000";
+    let jump_hosts = vec!["127.0.0.1:2222".to_string()];
+    let service_addr = "127.0.0.1:8080";
+
+    let ssh_tasks: Vec<_> = jump_hosts
+        .clone()
+        .into_iter()
+        .map(|ssh_addr| {
+            let cloned_config = config.clone();
+            let users = hosts_users.get(&ssh_addr).cloned().unwrap_or_default();
+            let mut server = helpers::SSHServer::new(Some(users));
+
+            task::spawn(async move {
+                let _ = server.run_on_address(cloned_config, ssh_addr.clone()).await;
+            })
+        })
+        .collect();
+    let service_handle = task::spawn(async move {
+        let serv = TcpListener::bind(service_addr).await.unwrap();
+        loop {
+            let (mut socket, _) = serv.accept().await.unwrap();
+            socket.write_all(b"hello world!").await.unwrap();
+        }
+    });
+
+    bind(
+        bind_addr,
+        jump_hosts,
+        service_addr,
+        sopsfile_path.to_str().unwrap(),
+    );
+
+    let mut conn = TcpStream::connect(bind_addr).await.unwrap();
+    let mut buf = vec![0; 1024];
+    let n = conn.read(&mut buf).await?;
+    let response = String::from_utf8_lossy(&buf[..n]).to_string();
+    println!("Received: {}", response);
+    assert_eq!(response, "hello world!");
+
+    unbind(bind_addr);
+    for task in ssh_tasks {
+        task.abort();
+    }
+    service_handle.abort();
+    Ok(())
+}
