f5cabdd8f05f215d784be4c3376763a431186dde
Maximilian Philipp <maxkon2000@gmail.com>
2025-08-25 13:14:01 +0200

    test: wireup failing low-session-bandwidth iperf3 test
    Signed-off-by: Maximilian Philipp <maxkon2000@gmail.com>

--- integration-tests/default.nix
+++ integration-tests/default.nix
@@ -2,5 +2,6 @@
   simple = pkgs.callPackage ./simple {inherit pkgs;};
   cli = pkgs.callPackage ./cli {inherit pkgs;};
   performance = pkgs.callPackage ./performance {inherit pkgs;};
+  low-session-bandwidth = pkgs.callPackage ./low-session-bandwidth {inherit pkgs;};
   # complex = pkgs.callPackage ./complex {inherit pkgs;};
 }
